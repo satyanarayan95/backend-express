9df2c9fa2fa9304f4eedbddf9af13cd4305e557a
satyanarayan95 <satyanarayan.patra5495@gmail.com>
2021-09-24 20:12:25 +0530

    added router

--- server.js
+++ server.js
@@ -1,12 +1,27 @@
 const express=require('express');
 const app=express();
 
+
+
 app.listen('5000',function(){
     console.log('server listening on port 5000');
 });
 
 app.use(express.json());
 
+const userRouter=express.Router();
+app.use('/user',userRouter);
+//mounting in express
+userRouter
+.route('/')
+.get(getUser)
+.post(createUser)
+.patch(updateUser)
+.delete(deleteUser);
+
+userRouter
+.route('/:id')
+.get(getUserById);
 
 let user={};
 // client <- server
@@ -14,18 +29,23 @@ app.get('/',(req,res)=>{
     res.send('Home Page');
 });
 
+//read
 app.get('/user',(req,res)=>{
     res.json(user);
 });
 
 //post request
 // client-> server 
+
+//create
 app.post('/user',(req,res)=>{
     user=req.body;
     // console.log(req.body);
     res.send('data has been added succesfully');
 });
 
+
+//update
 app.patch('/user',(req,res)=>{
     let obj=req.body;
     for(let key in obj){
@@ -34,7 +54,14 @@ app.patch('/user',(req,res)=>{
     res.json(user);
 });
 
+//delete
 app.delete('/user',(req,res)=>{
     user={};
     res.json(user);
 })
+
+//param route
+app.get('/user/:id',(req,res)=>{
+    console.log(req.params);
+    res.json(req.params.id);
+});
