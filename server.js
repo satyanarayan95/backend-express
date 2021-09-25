fec473c34ca3051e27e67b0b28a067b908bcb92d
satyanarayan95 <satyanarayan.patra5495@gmail.com>
2021-09-25 09:17:08 +0530

    plan.json added

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express=require('express');
+
 const app=express();
-
-
-
+// const router=express.Router();
 app.listen('5000',function(){
     console.log('server listening on port 5000');
 });
 
 app.use(express.json());
-app.use(express.static('public'));
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware');
+//     next();
+// });
 
+app.use(express.static('public'));
 const userRouter=express.Router();
 const authRouter=express.Router();
 
 app.use('/user',userRouter);
-app.use('/auth',authRouter)
+app.use('/auth',authRouter);
 //mounting in express
+
+
 userRouter
 .route('/')
 .get(getUser)
@@ -23,45 +29,97 @@ userRouter
 .patch(updateUser)
 .delete(deleteUser);
 
+async function getUser(req,res){
+    console.log('getUser called');
+    res.json(user);  
+}
+
+//post request
+// client-> server 
+//create
+// app.post('/user',createUser);
+function createUser(req,res){
+    user=req.body;
+    // console.log(req.body);
+    res.send('data has been added succesfully');
+}
+//update
+// app.patch('/user',updateUser);
+function updateUser (req,res){
+    let obj=req.body;
+    for(let key in obj){
+        user[key]=obj[key];
+    }
+    res.json(user);
+};
+//delete 
+// app.delete('/user',deleteUser);
+function deleteUser(req,res){
+    user={};
+    res.json(user);
+    // res.send('ussr has been deleted');
+}
+//param route
+// app.get('/user/:id',getUserById);
+
+function getUserById(req,res){
+    console.log(req.params);
+    res.json(req.params.id);
+}
+// app.use((req,res,next)=>{
+//     //do some work
+//     console.log('i am a middleware 2nd time');
+//     next();
+// });
+
 userRouter
 .route('/:id')
 .get(getUserById);
 
 authRouter
 .route('/signup')
-.post(signupUser);
+.post(setCreatedAt,signupUser);
+
+function setCreatedAt(req,res,next){
+    let obj=req.body;
+    //keys ka arr -> uska length
+    let length=Object.keys(obj).length;
+    if(length==0){
+        return res.status(400).json({message:"cannot create user if req.body is empty"})
+    }
+    req.body.createdAt=new Date().toISOString();
+    next();
+}
+const userModel=require('./models/userModel');
+async function signupUser(req,res){
+    // let userDetails=req.body;
+    // let name=userDetails.name;
+    // let email=userDetails.email;
+    // let password=userDetails.password;
+    try{
+        let userObj=req.body;
+        // user.push({email,name,password});
+        //put all data in mongo db
+        // create document in userModel
+        let user=await userModel.create(userObj);
+        console.log('user',user);
+        res.json({
+            message:'user signedUp',
+            user:userObj
+        });
+    }
+    catch(err){
+        console.log(err);
+        res.json({message: err.message})
+    }
+}
+
+
 
 authRouter
 .route('/forgetPassword')
 .get(getForgetPassword)
 .post(postForgetPassword,validateEmail);
-
-
-//redirects
-app.get('/user-all',(req,res)=>{
-    res.redirect('/user');
-});
-
-//404 page
-app.use((req,res)=>{
-    res.sendFile('public/404.html',{root:__dirname})
-});
-
-
-function signupUser(req,res){
-    // let userDetails=req.body;
-    // let name=userDetails.name;
-    // let email=userDetails.email;
-    // let password=userDetails.password;
-
-    let{email,name,password}=req.body;
-    user.push({email,name,password});
-    console.log('user',req.body);
-    res.json({
-        message:'user signedUp',
-        user:req.body
-    });
-}
 
 function getForgetPassword(req,res){
     res.sendFile('./public/forgetPassword.html',{root:__dirname});
@@ -90,47 +148,33 @@ function validateEmail(req,res){
         });
 }
 
+//redirects
+app.get('/user-all',(req,res)=>{
+    res.redirect('/user');
+});
 
-let user=[];
+//404 page
+app.use((req,res)=>{
+    res.sendFile('public/404.html',{root:__dirname})
+});
 
+// let user=[];
 // client <- server
-app.get('/',(req,res)=>{
-    res.send('Home Page');
-});
-
+//crud- create read update delete
 //read
-app.get('/user',(req,res)=>{
-    res.json(user);
-});
+// app.get('/',(req,res)=>{
+//     res.send('Home Page');
+// });
 
-//post request
-// client-> server 
-
-//create
-app.post('/user',(req,res)=>{
-    user=req.body;
-    // console.log(req.body);
-    res.send('data has been added succesfully');
-});
+// app.get('/user',getUser);
 
 
-//update
-app.patch('/user',(req,res)=>{
-    let obj=req.body;
-    for(let key in obj){
-        user[key]=obj[key];
-    }
-    res.json(user);
-});
 
-//delete
-app.delete('/user',(req,res)=>{
-    user={};
-    res.json(user);
-})
 
-//param route
-app.get('/user/:id',(req,res)=>{
-    console.log(req.params);
-    res.json(req.params.id);
-});
+
+
+
+
+
+
+
